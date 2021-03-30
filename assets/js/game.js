5b1d48c9c2b589bed0c2510b7a569324fa11f71a
Thijs <tc.herman@hotmail.com>
2021-03-30 09:25:39 +0200

    Game class added

---
+++ assets/js/game.js
@@ -0,0 +1,16 @@
+class Game {
+    constructor() {
+        console.log("Test Game");
+        this.update();
+        this.draw();
+
+    }
+    update() {
+        console.log("update")
+
+    }
+
+    draw() {
+        console.log("draw")
+    }
+}
