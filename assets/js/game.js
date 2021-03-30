367ad909f9259460091005ba2f022a3f21bda80d
Thijs <tc.herman@hotmail.com>
2021-03-30 11:39:42 +0200

    GameClass and Gameloop added

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,16 +1,36 @@
 class Game {
     constructor() {
         console.log("Test Game");
-        this.update();
-        this.draw();
+
+
+        //bepaald de framerate (niet aller beste manier maar wel makkelijkste)
+        // let me = this;
+        // setInterval(function() {
+        //     me.update();
+        //     me.draw();
+        // }, 1000/60);
+
+        //Gameloop
+        let lastTime;
+        const callback = (milliseconds) => {
+            if (lastTime) {
+                this.update((milliseconds - lastTime) / 1000);
+                this.draw();
+            }
+            lastTime = milliseconds;
+            window.requestAnimationFrame(callback);
+        }
+
+        callback();
 
     }
-    update() {
-        console.log("update")
+    update(deltatime) {
+        //console.log(deltatime);
+        //console.log("update")
 
     }
 
     draw() {
-        console.log("draw")
+        //console.log("draw")
     }
 }
