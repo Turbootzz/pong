e5dfc52390021c6d35f7e752fcbfd9877e0e2a98
Thijs <tc.herman@hotmail.com>
2021-04-13 13:00:23 +0200

    comments & collisions

--- assets/js/game.js
+++ assets/js/game.js
@@ -18,7 +18,12 @@ class Game {
             new Player(this.canvas.width - 20, this.canvas.height / 2, 2)
         ];
 
+        //shows score
+        this.hud = new Hud(this);
+
+        //array for the input
         this.keys = [];
+        //event for pressing key
         window.addEventListener('KEY_DOWN', (event) => {
             switch (event.detail) {
                 case 'ArrowUp': this.keys[38] = true;
@@ -31,7 +36,7 @@ class Game {
                     break;
             }
         });
-
+        //when letting go of key
         window.addEventListener('KEY_UP', (event) => {
             switch (event.detail) {
                 case 'ArrowUp': this.keys[38] = false;
@@ -72,29 +77,69 @@ class Game {
     }
 
     update(deltatime) {
+
+        //position of the ball changes on velocity
         this.ball.position.x += this.ball.velocity.x * deltatime;
         this.ball.position.y += this.ball.velocity.y * deltatime;
 
+        //position of player changes based on velocity
         this.players[0].position.y += this.players[0].velocity.y * deltatime;
         this.players[1].position.y += this.players[1].velocity.y * deltatime;
 
+        //checks the players input
         this.players.forEach(player => this.checkInput(player, this.ball));
 
-        if (this.ball.bottom > this.canvas.height || this.ball.top < 0) {
+        this.checkCollisions(this.players, this.ball, this.hud.edges, deltatime);
+    }
+
+    checkCollisions(players, ball, edges, deltatime) {
+
+        for(let p=0; p<players.length; p++) {
+            if(players[p].top < edges[0].bottom) {
+                players[p].position.y = edges[0].size.y + (players[p].size.y/2);
+            }else if(players[p].bottom > edges[1].top) {
+                players[p].position.y = this.canvas.height - edges[1].size.y - (players[p].size.y/2);
+            }
+        }
+
+        
+
+        //checks if the ball hits the top or the bottom.
+        if (this.ball.bottom > this.canvas.height-10 || this.ball.top < 10) {
             this.ball.velocity.y = -this.ball.velocity.y;
         }
 
-        if (this.ball.right > this.canvas.width || this.ball.left < 0) {
-            this.ball.velocity.x = -this.ball.velocity.x;
+
+        //checks if ball hits the player
+        if(this.collide(ball, players[0], deltatime)) {
+            alert('bots!');
         }
 
+
+        //
     }
 
+    collide(rect1, rect2, dt) {
+        if (rect1.left   + rect1.velocity.x * dt < rect2.right  + rect2.velocity.x * dt &&
+            rect1.right  + rect1.velocity.x * dt > rect2.left   + rect2.velocity.x * dt &&
+            rect1.top    + rect1.velocity.y * dt < rect2.bottom + rect2.velocity.y * dt &&
+            rect1.bottom + rect1.velocity.y * dt > rect2.top    + rect2.velocity.y * dt) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
+
     draw() {
+        //clears the context
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-        // Tekenen bal
+
+        // draws ball
         this.drawRectangle(this.context, this.ball);
 
+        // Draws players
         for (let i = 0; i < this.players.length; i++) {
             this.drawRectangle(this.context, this.players[i]);
         }
@@ -102,7 +147,9 @@ class Game {
     }
 
     drawRectangle(ctx, rect, color = 'white') {
+        // choose the colour
         ctx.fillStyle = color;
+        // draws a rectangle on the canvas
         ctx.fillRect(rect.left, rect.top, rect.size.x, rect.size.y);
     }
 }
